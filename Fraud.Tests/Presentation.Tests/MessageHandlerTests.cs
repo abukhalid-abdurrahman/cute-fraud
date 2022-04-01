0a1efeb0f6cd6f96de4d07d2786553f46af50a5a
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-01 11:57:56 +0500

    added message notifier and handler tests

---
+++ Fraud.Tests/Presentation.Tests/MessageHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fraud.Tests.Presentation.Tests
+{
+    [TestFixture]
+    public class MessageHandlerTests
+    {
+        [Test]
+        public async Task MessageHandler_HandleMessage_ShouldHandleMessageSuccessfully()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Fraud.Tests/Presentation.Tests/MessageNotifierTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fraud.Tests.Presentation.Tests
+{
+    [TestFixture]
+    public class MessageNotifierTests
+    {
+        [Test]
+        public async Task MessageNotifier_Notify_NotifyMessageSuccessfully()
+        {
+            Assert.Pass();
+        }
+    }
+}
