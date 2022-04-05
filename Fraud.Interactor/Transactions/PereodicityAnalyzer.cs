8278158af947e3739d4900b2f48d4613c377f7a7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-05 15:41:21 +0500

    added pereodicity analzyer

---
+++ Fraud.Interactor/Transactions/PereodicityAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Fraud.Entities.DTOs;
+using Fraud.Entities.Models;
+using Fraud.UseCase.Transactions;
+
+namespace Fraud.Interactor.Transactions
+{
+    public class PeriodicityAnalyzer : ITransactionAnalyzer
+    {
+        private readonly int _fraudPriorityStep = 30;
+        
+        public TransactionAnalyzerResult AnalyzeTransactions(in Transaction[] transactions)
+        {            
+            if (transactions == null)
+                throw new ArgumentNullException(nameof(transactions));
+
+            var fraudPriority = 0;
+            var iterationsCount = transactions.Length - 1;
+
+            for (var i = 0; i < iterationsCount; i++)
+            {
+                var nextTransactionIndex = i + 1;
+                if(nextTransactionIndex > iterationsCount)
+                    break;
+                
+                var transactionInterval = 
+                    Math.Abs(transactions[nextTransactionIndex].DateCreatedUnix - transactions[i].DateCreatedUnix);
+                if (transactionInterval <= 60)
+                    fraudPriority += _fraudPriorityStep;
+            }
+            
+            return new TransactionAnalyzerResult(fraudPriority, transactions[0].CardToken);
+        }
+    }
+}
