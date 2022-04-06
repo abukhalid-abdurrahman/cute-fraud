34a190a4ed101f447cc9b1a809c2e419011f1d94
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-06 09:56:56 +0500

    implemented count analyzer

--- Fraud.Interactor/Transactions/CountAnalyzer.cs
+++ Fraud.Interactor/Transactions/CountAnalyzer.cs
@@ -1,24 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using Fraud.Entities.DTOs;
 using Fraud.Entities.Models;
 using Fraud.UseCase.Transactions;
 
 namespace Fraud.Interactor.Transactions
 {
-    public class PeriodicityAnalyzer : ITransactionAnalyzer
+    public class CountAnalyzer : ITransactionAnalyzer
     {
         private readonly int _fraudPriorityStep = 30;
-        private readonly int _transactionsIntervalInSeconds = 60;
 
         public TransactionAnalyzerResult AnalyzeTransactions(in Transaction[] transactions)
-        {            
+        {
             if (transactions == null)
                 throw new ArgumentNullException(nameof(transactions));
 
             var fraudPriority = 0;
             var iterationsCount = transactions.Length - 1;
 
+            var transactionIntervals = new long[transactions.Length - 1];
+            
             for (var i = 0; i < iterationsCount; i++)
             {
                 var nextTransactionIndex = i + 1;
@@ -27,10 +28,16 @@ namespace Fraud.Interactor.Transactions
                 
                 var transactionInterval = 
                     Math.Abs(transactions[nextTransactionIndex].DateCreatedUnix - transactions[i].DateCreatedUnix);
-                if (transactionInterval <= _transactionsIntervalInSeconds)
+                transactionIntervals[i] = transactionInterval;
+            }
+
+            var intervalAvg = transactionIntervals.Average();
+            for (var i = 0; i < transactionIntervals.Length - 1; i++)
+            {
+                if (transactionIntervals[i] < intervalAvg)
                     fraudPriority += _fraudPriorityStep;
             }
-            
+
             return new TransactionAnalyzerResult(fraudPriority, transactions[0].CardToken);
         }
     }
