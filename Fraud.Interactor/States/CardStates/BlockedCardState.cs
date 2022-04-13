56953dbe6fa11aec006859541a4c5518373faeac
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-13 09:51:09 +0500

    notifieng other services about card state through rabbit mq

--- Fraud.Interactor/States/CardStates/BlockedCardState.cs
+++ Fraud.Interactor/States/CardStates/BlockedCardState.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Fraud.Entities.Enums;
 using Fraud.Entities.Models;
+using Fraud.Interactor.MessageBroking;
+using Fraud.Presentation.Configurations;
 using Fraud.UseCase.Cards;
+using Fraud.UseCase.MessageBroking;
+using Newtonsoft.Json;
 
 namespace Fraud.Interactor.States.CardStates
 {
@@ -17,7 +22,11 @@ namespace Fraud.Interactor.States.CardStates
         
         public async Task HandleState()
         {
-            throw new System.NotImplementedException();
+            var cardStateMessage = JsonConvert.SerializeObject(this.Card);
+            // TODO: Change implementation instance to interface instance
+            IMessageBrokerService messageBrokerService = new RabbitMqMessageBrokerService(new RabbitMqConfigurations());
+            messageBrokerService
+                .Send(routingKey: "some-routing-key", exchangeName: "some-ex-name", message: cardStateMessage);
         }
     }
 }
--- Fraud.Interactor/States/CardStates/SuspiciousCardState.cs
+++ Fraud.Interactor/States/CardStates/SuspiciousCardState.cs
@@ -1,7 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Fraud.Entities.Enums;
 using Fraud.Entities.Models;
+using Fraud.Interactor.MessageBroking;
+using Fraud.Presentation.Configurations;
 using Fraud.UseCase.Cards;
+using Fraud.UseCase.MessageBroking;
+using Newtonsoft.Json;
 
 namespace Fraud.Interactor.States.CardStates
 {
@@ -17,7 +21,10 @@ namespace Fraud.Interactor.States.CardStates
         
         public async Task HandleState()
         {
-            throw new System.NotImplementedException();
-        }
+            var cardStateMessage = JsonConvert.SerializeObject(this.Card);
+            // TODO: Change implementation instance to interface instance
+            IMessageBrokerService messageBrokerService = new RabbitMqMessageBrokerService(new RabbitMqConfigurations());
+            messageBrokerService
+                .Send(routingKey: "some-routing-key", exchangeName: "some-ex-name", message: cardStateMessage);        }
     }
 }
